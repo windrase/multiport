3384429e4d3fb5c36c503c8c6b8c05bc9f401065
WindraSe <pemaingamez123@gmail.com>
2026-02-17 05:43:59 +0700

    Update limit.sh

--- files/limit.sh
+++ files/limit.sh
@@ -1,17 +1,22 @@
 #!/bin/bash
-REPO="https://raw.githubusercontent.com/arivpnstores/v4/main/"
-wget -q -O /etc/systemd/system/limitvmess.service "${REPO}Fls/limitvmess.service" && chmod +x limitvmess.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitvless.service "${REPO}Fls/limitvless.service" && chmod +x limitvless.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limittrojan.service "${REPO}Fls/limittrojan.service" && chmod +x limittrojan.service >/dev/null 2>&1
-wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPO}Fls/limitshadowsocks.service" && chmod +x limitshadowsocks.service >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vmess "${REPO}Fls/vmess" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.vless "${REPO}Fls/vless" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.trojan "${REPO}Fls/trojan" >/dev/null 2>&1
-wget -q -O /etc/xray/limit.shadowsocks "${REPO}Fls/shadowsocks" >/dev/null 2>&1
+REPO="https://raw.githubusercontent.com/windrase/multiport/main/"
+
+wget -q -O /etc/systemd/system/limitvmess.service "${REPO}files/limitvmess.service" && chmod +x /etc/systemd/system/limitvmess.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitvless.service "${REPO}files/limitvless.service" && chmod +x /etc/systemd/system/limitvless.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limittrojan.service "${REPO}files/limittrojan.service" && chmod +x /etc/systemd/system/limittrojan.service >/dev/null 2>&1
+wget -q -O /etc/systemd/system/limitshadowsocks.service "${REPO}files/limitshadowsocks.service" && chmod +x /etc/systemd/system/limitshadowsocks.service >/dev/null 2>&1
+
+mkdir -p /etc/xray
+wget -q -O /etc/xray/limit.vmess "${REPO}files/vmess" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.vless "${REPO}files/vless" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.trojan "${REPO}files/trojan" >/dev/null 2>&1
+wget -q -O /etc/xray/limit.shadowsocks "${REPO}files/shadowsocks" >/dev/null 2>&1
+
 chmod +x /etc/xray/limit.vmess
 chmod +x /etc/xray/limit.vless
 chmod +x /etc/xray/limit.trojan
 chmod +x /etc/xray/limit.shadowsocks
+
 systemctl daemon-reload
 systemctl enable --now limitvmess
 systemctl enable --now limitvless
